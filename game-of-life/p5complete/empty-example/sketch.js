c4bc581c90e8236c4ba6d28d8631dac7d2f2cafb
Cody Holman <holman.cody@gmail.com>
2020-05-27 19:40:14 -0500

    positioning

--- game-of-life/p5complete/empty-example/sketch.js
+++ game-of-life/p5complete/empty-example/sketch.js
@@ -6,13 +6,15 @@ function make2DArray(cols, rows) {
   return arr;
 }
 let grid;
-let res = 20;
+let res = 25;
 let cols;
 let rows;
 
 function setup() {
   // put setup code here
-  createCanvas(600, 400);
+  let canvas = createCanvas(500, 500);
+  canvas.parent('canvas');
+
   cols = width / res;
   rows = height / res;
 
@@ -26,45 +28,48 @@ function setup() {
 
 function draw() {
   background(0);
-
-  for (let i = 0; i < cols; i++){
-    for (let j = 0; j < rows; j++){
-      let x = i * res;
-      let y = j * res;
-      if(grid[i][j] == 1){
-        fill(255);
-        stroke(0);
-        rect(x, y, res - 1 , res - 1);
-      } 
+  let start = false;
+  if(start == true){
+    for (let i = 0; i < cols; i++){
+      for (let j = 0; j < rows; j++){
+        let x = i * res;
+        let y = j * res;
+        if(grid[i][j] == 1){
+          fill(255);
+          stroke(0);
+          rect(x, y, res - 1 , res - 1);
+        } 
+      }
     }
-  }
+  
 
   let next = make2DArray(cols, rows);
 
   //compute next based on grid
-  for(let i = 0; i < cols; i++){
-    for(let j = 0; j < cols; j++){
-      let state = grid[i][j];
-      //edges
-      // if (i == 0 || i == cols - 1 || j == 0 || j == rows -1){
-      //   next[i][j] = state
-      // } else {
+    for(let i = 0; i < cols; i++){
+      for(let j = 0; j < cols; j++){
+        let state = grid[i][j];
+        //edges
+        // if (i == 0 || i == cols - 1 || j == 0 || j == rows -1){
+        //   next[i][j] = state
+        // } else {
 
-      //count live neighbors
-      let sum = 0;
-      let neighbors = countNeighbors(grid, i, j);
+        //count live neighbors
+        let sum = 0;
+        let neighbors = countNeighbors(grid, i, j);
 
-      if (state == 0 && neighbors == 3){
-        next[i][j] = 1;
-      } else if (state == 1 && neighbors < 2 || neighbors > 3){
-        next[i][j] = 0;
-      }else{
-        next[i][j] = state;
+        if (state == 0 && neighbors == 3){
+          next[i][j] = 1;
+        } else if (state == 1 && neighbors < 2 || neighbors > 3){
+          next[i][j] = 0;
+        }else{
+          next[i][j] = state;
+        }
       }
-  }
-}
+    } 
 
-grid = next;
+  grid = next;
+  }
 }
 
 function countNeighbors(grid, x, y) {
